4812c538148e8c501eac63693f03adf6af2d7f5e
roman <roma_lasov@mal.ru>
2024-05-19 20:15:55 +0100

    p1

--- src/components/popups/PopExit/PopExit.jsx
+++ src/components/popups/PopExit/PopExit.jsx
@@ -1,37 +1,38 @@
 import { Link, useNavigate } from "react-router-dom"
 import { appRoutes } from "../../../lib/appRoutes"
 import { useUser } from "../../../hooks/useUser"
+import * as S from "./PopExit.styled"
 
 export default function PopExit() {
     const { logout } = useUser()
     const navigate = useNavigate()
     return (
-        <div className="pop-exit" id="popExit">
-            <div className="pop-exit__container">
-                <div className="pop-exit__block">
-                    <div className="pop-exit__ttl">
-                        <h2>Выйти из аккаунта?</h2>
-                    </div>
-                    <form className="pop-exit__form" id="formExit" action="#">
-                        <div className="pop-exit__form-group">
+        <S.PopExit id="popExit">
+            <S.PopExitContainer>
+                <S.PopExitBlock>
+                    <S.PopExitTitle>
+                        Выйти из аккаунта?
+                    </S.PopExitTitle>
+                    {/* <form className="pop-exit__form" id="formExit" action="#"> */}
+                    <S.PopExitFormGroup>
 
-                            <span onClick={() => {
-                                logout();
-                                navigate(appRoutes.SIGNIN);
-                            }} className="pop-exit__exit-yes _hover01" id="exitYes">
-                                Да, выйти
-                            </span>
+                        <S.PopExitYes onClick={() => {
+                            logout();
+                            navigate(appRoutes.SIGNIN);
+                        }} id="exitYes">
+                            Да, выйти
+                        </S.PopExitYes>
 
-                            <Link to={appRoutes.MAIN}>
-                                <span className="pop-exit__exit-no _hover03" id="exitNo">
-                                    Нет, остаться
-                                </span>
-                            </Link>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+                        <Link to={appRoutes.MAIN}>
+                            <S.PopExitNo id="exitNo">
+                                Нет, остаться
+                            </S.PopExitNo>
+                        </Link>
+                    </S.PopExitFormGroup>
+                    {/* </form> */}
+                </S.PopExitBlock>
+            </S.PopExitContainer>
+        </S.PopExit>
     )
 }
 
